f9bb2c55b12055c85eb9ffbf8b612c9e452041ed
Karyana Yandi <xkaryanayandi@gmail.com>
2024-04-14 18:34:26 +0700

    fix trpc

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -3,7 +3,7 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch"
 
 import { appRouter, createContext } from "@/server"
 
-export const all: APIRoute = ({ request }) => {
+export const ALL: APIRoute = ({ request }) => {
   return fetchRequestHandler({
     endpoint: "/api/trpc",
     req: request,
