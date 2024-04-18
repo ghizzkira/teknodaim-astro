a63477991603ab86495a5d14d2bfd29920239029
ghizzkira <tiadiagis@gmail.com>
2024-04-18 14:17:57 +0700

    feat: add prerender false

--- .vercel/output/functions/_render.func/home/siga/Documents/teknodaim-astro/.vercel/output/_functions/chunks/_.._C9x1KD_V.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/__MLL9IRBw.mjs').then(n => n._);
-
-export { page };
--- .vercel/output/functions/_render.func/home/siga/Documents/teknodaim-astro/.vercel/output/_functions/chunks/sign-in_DwVmlwL-.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/sign-in_Ga6102nY.mjs');
-
-export { page };
