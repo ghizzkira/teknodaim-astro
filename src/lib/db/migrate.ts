12af431ab6a6a5b09407cef127aecfc3bd1668a4
Karyana Yandi <xkaryanayandi@gmail.com>
2024-04-20 13:50:15 +0700

    add migrate command

--- src/lib/db/migrate.ts
+++ src/lib/db/migrate.ts
@@ -5,14 +5,17 @@ import { migrate } from "drizzle-orm/libsql/migrator"
 async function main() {
   const db = drizzle(
     createClient({
-      url: import.meta.env.DATABASE_URL,
-      authToken: import.meta.env.DATABASE_AUTH_TOKEN,
+      // url: import.meta.env.DATABASE_URL,
+      // authToken: import.meta.env.DATABASE_AUTH_TOKEN,
+      url: "libsql://teknodaim-new-dafundateam.turso.io",
+      authToken:
+        "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTM0MTg3MDQsImlkIjoiMzExMDYwN2ItMDI0MS00YTNmLTk0ZDktODBlNzhmMmMxMTVhIn0.6dX_OQG3iZ71sRvE79oIXkIY0rUQrGnHoRn2fnqF8ALQZHUyo-yHmD3hBgTZZ2d_wBkZru-AWcvqgJySx8xMCw",
     }),
   )
 
   console.log("Running migrations")
 
-  await migrate(db, { migrationsFolder: "drizzle" })
+  await migrate(db, { migrationsFolder: "src/lib/db/migrations" })
 
   console.log("Migrated successfully")
 
