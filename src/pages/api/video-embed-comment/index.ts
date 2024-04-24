54df9d4d7e3dc4d753e1b51f8733cd27a2e82430
karyanayandi <halo@yandi.me>
2024-04-24 17:09:21 +0700

    add all comment api

---
+++ src/pages/api/video-embed-comment/index.ts
@@ -0,0 +1,35 @@
+import type { APIContext, APIRoute } from "astro"
+import { z } from "zod"
+
+import { createVideoEmbedComment } from "@/lib/action/video-embed-comment"
+import { createVideoEmbedCommentSchema } from "@/lib/validation/video-embed-comment"
+
+export const POST: APIRoute = async (context: APIContext) => {
+  try {
+    const user = context.locals.user
+
+    const body = await context.request.json()
+    const parsedInput = createVideoEmbedCommentSchema.parse(body)
+    const data = await createVideoEmbedComment({
+      authorId: user?.id!,
+      ...parsedInput,
+    })
+
+    if (!data) {
+      return new Response(null, {
+        status: 404,
+      })
+    }
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    })
+  } catch (error) {
+    console.error(error)
+    if (error instanceof z.ZodError) {
+      return new Response(error.errors[0].message, { status: 422 })
+    }
+    return new Response("Internal Server Error", { status: 500 })
+  }
+}
---
+++ src/pages/api/wp-comment/index.ts
@@ -0,0 +1,35 @@
+import type { APIContext, APIRoute } from "astro"
+import { z } from "zod"
+
+import { createWpComment } from "@/lib/action/wp-comment"
+import { createWpCommentSchema } from "@/lib/validation/wp-comment"
+
+export const POST: APIRoute = async (context: APIContext) => {
+  try {
+    const user = context.locals.user
+
+    const body = await context.request.json()
+    const parsedInput = createWpCommentSchema.parse(body)
+    const data = await createWpComment({
+      authorId: user?.id!,
+      ...parsedInput,
+    })
+
+    if (!data) {
+      return new Response(null, {
+        status: 404,
+      })
+    }
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    })
+  } catch (error) {
+    console.error(error)
+    if (error instanceof z.ZodError) {
+      return new Response(error.errors[0].message, { status: 422 })
+    }
+    return new Response("Internal Server Error", { status: 500 })
+  }
+}
