e3b3aa7730ea4f65f8328a26d6e564e057c4eb22
karyanayandi <halo@yandi.me>
2024-04-29 12:58:56 +0700

    fix how to read env from drizzle

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,8 +4,8 @@ import { drizzle } from "drizzle-orm/libsql"
 import * as schema from "./schema"
 
 export const sqlite = createClient({
-  url: import.meta.env.DATABASE_URL,
-  authToken: import.meta.env.DATABASE_AUTH_TOKEN,
+  url: import.meta.env.VITE_DATABASE_URL,
+  authToken: import.meta.env.VITE_DATABASE_AUTH_TOKEN,
 })
 
 export const initializeDB = drizzle(sqlite)
