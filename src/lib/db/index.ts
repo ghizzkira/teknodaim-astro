0d84334880a2f08591895cc90ce3d23b15d37c91
karyanayandi <halo@yandi.me>
2024-04-29 13:20:47 +0700

    fix how to handle env on driazzle config

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,14 +1,12 @@
-import "dotenv/config"
-
 import { createClient } from "@libsql/client"
 import { drizzle } from "drizzle-orm/libsql"
 
 import * as schema from "./schema"
 
-export const sqlite = createClient({
-  url: process.env.VITE_DATABASE_URL!,
-  authToken: process.env.VITE_DATABASE_AUTH_TOKEN,
+export const client = createClient({
+  url: import.meta.env.VITE_DATABASE_URL!,
+  authToken: import.meta.env.VITE_DATABASE_AUTH_TOKEN!,
 })
 
-export const initializeDB = drizzle(sqlite)
-export const db = drizzle(sqlite, { schema })
+export const initializeDB = drizzle(client)
+export const db = drizzle(client, { schema })
