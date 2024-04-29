aabcbc365d0288824ba444d5489bec980875e922
karyanayandi <halo@yandi.me>
2024-04-29 13:49:34 +0700

    use function on turso db client

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,12 +1,15 @@
+import "dotenv/config"
+
 import { createClient } from "@libsql/client"
 import { drizzle } from "drizzle-orm/libsql"
 
 import * as schema from "./schema"
 
-export const client = createClient({
-  url: import.meta.env.VITE_DATABASE_URL!,
-  authToken: import.meta.env.VITE_DATABASE_AUTH_TOKEN!,
-})
+export const client = () =>
+  createClient({
+    url: import.meta.env.VITE_DATABASE_URL!,
+    authToken: import.meta.env.VITE_DATABASE_AUTH_TOKEN!,
+  })
 
-export const initializeDB = drizzle(client)
-export const db = drizzle(client, { schema })
+export const initializeDB = drizzle(client())
+export const db = drizzle(client(), { schema })
