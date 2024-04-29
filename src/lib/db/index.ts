28e111246c7f2d6e79ecccc2a131273910cec9ed
karyanayandi <halo@yandi.me>
2024-04-29 13:01:40 +0700

    fix read db with dotenv

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,11 +1,13 @@
+import "dotenv/config"
+
 import { createClient } from "@libsql/client"
 import { drizzle } from "drizzle-orm/libsql"
 
 import * as schema from "./schema"
 
 export const sqlite = createClient({
-  url: import.meta.env.VITE_DATABASE_URL,
-  authToken: import.meta.env.VITE_DATABASE_AUTH_TOKEN,
+  url: process.env.VITE_DATABASE_URL!,
+  authToken: process.env.VITE_DATABASE_AUTH_TOKEN,
 })
 
 export const initializeDB = drizzle(sqlite)
