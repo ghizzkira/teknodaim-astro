ba99843a61f6dd41060afb2809d64ef8c512a2d3
karyanayandi <halo@yandi.me>
2024-04-29 13:34:46 +0700

    revert

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@libsql/client"
-import { drizzle, type LibSQLDatabase } from "drizzle-orm/libsql"
+import { drizzle } from "drizzle-orm/libsql"
 
 import * as schema from "./schema"
 
@@ -8,28 +8,5 @@ export const client = createClient({
   authToken: import.meta.env.VITE_DATABASE_AUTH_TOKEN!,
 })
 
-export function dbClient(): LibSQLDatabase<typeof schema> {
-  const url = import.meta.env.VITE_DATABASE_URL?.trim()
-  if (url === undefined) {
-    throw new Error("VITE_DATABASE_URL is not defined")
-  }
-
-  const authToken = import.meta.env.VITE_DATABASE_AUTH_TOKEN?.trim()
-  if (authToken === undefined) {
-    if (!url.includes("file:")) {
-      throw new Error("VITE_DATABASE_AUTH_TOKEN is not defined")
-    }
-  }
-
-  return drizzle(
-    createClient({
-      url,
-      authToken,
-    }),
-    { schema },
-  )
-}
-
 export const initializeDB = drizzle(client)
-export const db = dbClient()
-// export const db = drizzle(client, { schema })
+export const db = drizzle(client, { schema })
