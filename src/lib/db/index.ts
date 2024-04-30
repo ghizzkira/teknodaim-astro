88ed73efbe2204e61fa24218eb19a269b5f59ff7
karyanayandi <halo@yandi.me>
2024-04-30 12:43:42 +0700

    return drizzle

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,6 +2,10 @@ import { drizzle } from "drizzle-orm/d1"
 
 import * as schema from "./schema"
 
-export const initializeDBWithoutSchema = (DB: D1Database) => drizzle(DB)
+export const initializeDBWithoutSchema = (DB: D1Database) => {
+  return drizzle(DB)
+}
 
-export const initializeDB = (DB: D1Database) => drizzle(DB, { schema })
+export const initializeDB = (DB: D1Database) => {
+  return drizzle(DB, { schema })
+}
