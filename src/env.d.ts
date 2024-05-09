9ea5c54b6efa29e9084487328ff15dd15a7393f3
karyanayandi <halo@yandi.me>
2024-05-09 14:04:36 +0700

    fix env.d.ts eslint problem

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,13 @@
 /// <reference types="astro/client" />
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
 
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>
+type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<ENV>
 
 declare namespace App {
   interface Locals extends Runtime {
     session: import("lucia").Session | null
     user: import("lucia").User | null
-    cache(seconds: number): void
   }
 }
 
