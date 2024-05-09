288e0053a6422cdbb54ec09c00e7b7a601318faa
karyanayandi <halo@yandi.me>
2024-05-09 15:11:44 +0700

    fix env.d.ts runtime problem

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,9 @@
-/// <reference types="astro/client" />
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../.astro/types.d.ts" />
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 
-type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<ENV>
+/// <reference types="astro/client" />
+/// <reference path="../.astro/types.d.ts" />
 
 declare namespace App {
   interface Locals extends Runtime {
@@ -13,6 +14,8 @@ declare namespace App {
 
 type D1Database = import("@cloudflare/workers-types/experimental").D1Database
 
-interface ENV {
+type ENV = {
   DB: D1Database
 }
+
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>
