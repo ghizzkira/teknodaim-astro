625f341797bd07d5fc3905fb081e2ecd62c2bcbe
ghizzkira <tiadiagis@gmail.com>
2024-05-12 14:31:39 +0700

    fix change wrangler config

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -17,19 +17,38 @@ interface UploadImageToS3Props {
   contentType?: string
   width?: number
   height?: number
+  config: {
+    region: "auto" | string
+    accountId: string
+    bucket: string
+    credentials: {
+      accessKeyId: string
+      secretAccessKey: string
+    }
+  }
 }
 
 export async function uploadImageToR2({
   file,
   fileName,
   contentType = "image/webp",
+  config,
 }: UploadImageToS3Props): Promise<string> {
   const params = {
-    Bucket: import.meta.env.R2_BUCKET,
+    Bucket: config.bucket,
     Key: fileName,
     Body: file,
     ContentType: contentType,
   }
+  const r2Config = {
+    region: config.region,
+    endpoint: `https://${config.accountId}.r2.cloudflarestorage.com`,
+    credentials: {
+      accessKeyId: config.credentials.accessKeyId,
+      secretAccessKey: config.credentials.secretAccessKey,
+    },
+  }
+  const r2Client = new S3Client(r2Config)
 
   const command = new PutObjectCommand(params)
   await r2Client.send(command)
