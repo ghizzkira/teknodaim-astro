2fba043b98a82294a4998781b2c84641abde8f91
ghizzkira <tiadiagis@gmail.com>
2024-05-24 16:23:58 +0700

    undo

--- src/lib/wp/action/http.ts
+++ src/lib/wp/action/http.ts
@@ -7,14 +7,13 @@ export const wpHttp = async <T>(
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     },
     body: JSON.stringify({
       query,
       variables,
     }),
   }
+
   const url = new URL(import.meta.env.PUBLIC_WP_API ?? "")
 
   try {
@@ -29,7 +28,6 @@ export const wpHttp = async <T>(
     return [data as T, null]
   } catch (err) {
     console.error(url.href, err)
-    throw new Error(err as unknown as string)
     return [null, err as Error]
   }
 }
